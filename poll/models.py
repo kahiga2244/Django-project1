42baf56a4285caa17e99044078076c7906eca129
kahiga2244 <kahigakamiru@gmail.com>
2021-03-22 10:58:33 +0300

    adding the first class model

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    question = models.TextField()
+    Ruto_Samoei = models.CharField(max_length=30)
+    Raila_Amolo = models.CharField(max_length=30)
+    Mudavadi_Musalia = models.CharField(max_length=30)
+    Gideon_Moi = models.CharField(max_length=30)
+    Kivutha_Kibwana = models.CharField(max_length=30)
+    Kituyi_Mukhisa = models.CharField(max_length=30)
+
+    Ruto_Samoei_count = models.IntegerField(default=0)
+    Raila_Amolo_count = models.IntegerField(default=0)
+    Mudavadi_Musalia = models.IntegerField(default=0)
+    Gideon_Moi = models.IntegerField(default=0)
+    Kivutha_Kibwana = models.IntegerField(default=0)
+    Kituyi_Mukhisa = models.IntegerField(default=0)
+
