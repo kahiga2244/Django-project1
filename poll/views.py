efecfcc43ab4103996145bd9529a0d0446144891
kahiga2244 <kahigakamiru@gmail.com>
2021-03-22 11:09:33 +0300

    creating a home page/home/stone/Desktop/Python-work/poll-project/poll_project/poll/templates/home.html

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'home.html',context)
+
+def create(request):
+    context = {}
+    return render(request, 'create.html',context)
+
+def vote(request, poll_id):
+    context = {}
+    return render(request, 'vote.html', context)
+
+def results(request, poll_id):
+    context = {}
+    return render(request, 'results.html', context)
+
+
