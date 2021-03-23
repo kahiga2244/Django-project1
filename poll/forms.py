8d5f2b2871ec05cc841e4537e4d1b0679d98cbe9
kahiga2244 <kahigakamiru@gmail.com>
2021-03-23 13:29:27 +0300

    development branch

--- poll/forms.py
+++ poll/forms.py
@@ -1,8 +1,8 @@
-from django.forms import ModelForm
+# from django.forms import ModelForm
 
-from .models import Poll
+# from .models import Poll
 
-class CreatePollForm(ModelForm):
-    class Meta:
-        model = Poll
-        fields = ['question', 'option_one', '']
+# class CreatePollForm(ModelForm):
+#     class Meta:
+#         # model = Poll
+#         # fields = ['question', 'option_one', 'option_two','option_three','option_four','option_five','option_six']
--- poll/views.py
+++ poll/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-from .model import Poll
-from .forms import CreatePollForm
+from .models import Poll,User
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect
+
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'home.html',context)
 
-def create(request):
-    form = CreatePollForm()
-    context = {'form' : form }
-    return render(request, 'create.html',context)
-
-def vote(request, poll_id):
+# def create(request):
+#     # form = CreatePollForm()
+#     context = {'form' : form }
+#     return render(request, 'create.html',context)
+@csrf_protect
+def vote(request):
     context = {}
     return render(request, 'vote.html', context)
 
-def results(request, poll_id):
-    context = {}
-    return render(request, 'results.html', context)
+# def results(request, poll_id):
+#     context = {}
+#     return render(request, 'results.html', context)
 
 
