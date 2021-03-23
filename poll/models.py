864ceebe9f6c3111f9c30e937f223a024cb1f403
kahiga2244 <kahigakamiru@gmail.com>
2021-03-23 03:09:58 +0300

    adding class

--- poll/models.py
+++ poll/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Poll(models.Model):
-    question = models.TextField()
-    Ruto_Samoei = models.CharField(max_length=30)
-    Raila_Amolo = models.CharField(max_length=30)
-    Mudavadi_Musalia = models.CharField(max_length=30)
-    Gideon_Moi = models.CharField(max_length=30)
-    Kivutha_Kibwana = models.CharField(max_length=30)
-    Kituyi_Mukhisa = models.CharField(max_length=30)
+class User(models.Model):
+    user_id = models.IntegerField()
+    email = models.EmailField()
 
-    Ruto_Samoei_count = models.IntegerField(default=0)
-    Raila_Amolo_count = models.IntegerField(default=0)
-    Mudavadi_Musalia_count = models.IntegerField(default=0)
-    Gideon_Moi_count = models.IntegerField(default=0)
-    Kivutha_Kibwana_count = models.IntegerField(default=0)
-    Kituyi_Mukhisa_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.id
+    class Meta:
+        ordering = ['id']
+    
+    def save_user(self):
+        self.save()
+
+class Poll(models.Model):
+    president = models.CharField(max_length=30)
+    political_party = models.CharField(max_length=30)
+    votes = models.IntegerField(default=0)
+    poll_number = models.IntegerField(default=0)
+
+   
 
