e273311de70f3a2e64ecd9c467372f764409557e
kahiga2244 <kahigakamiru@gmail.com>
2021-03-23 12:42:32 +0300

    testing my user model to update

--- poll/models.py
+++ poll/models.py
@@ -20,9 +20,25 @@ class User(models.Model):
 
 class Poll(models.Model):
     president = models.CharField(max_length=30)
+    # user = models.ForeignKey(
+    #     User, 
+    #     on_delete=models.CASCADE,
+    #     related_name="Polls",
+    #     related_query_name = "poll"
+    # )
     political_party = models.CharField(max_length=30)
     votes = models.IntegerField(default=0)
     poll_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.president
+    class Meta:
+        ordering = ['president']
+    
+    def save_user(self):
+        self.save()
+    def delete_user(self):
+        self.delete()
 
 
    
