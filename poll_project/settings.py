77fb525b8c5c5b7b8c00f6a624b706ad817f7ce4
kahiga2244 <kahigakamiru@gmail.com>
2021-03-23 03:13:04 +0300

    connecting to the database

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'poll',
-    'bootstrap3'
+    'bootstrap3',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,10 @@ WSGI_APPLICATION = 'poll_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vote',
+        'USER': 'stone',
+        'PASSWORD':'mumbi2244',
     }
 }
 
