ecca33a3fab256881f2b04ede9ae194b4bac8f13
kahiga2244 <kahigakamiru@gmail.com>
2021-03-24 10:48:47 +0300

    admin panel

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,Poll
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    filter_horizontal =('user',)
+
+admin.site.register(User)
+admin.site.register(Poll)
+
